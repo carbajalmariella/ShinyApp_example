e543ad0cbe4b62c0eb8adf0dddb2be1a1a85f0f1
carbajalmariella@gmail.com <carbajalmariella@gmail.com>
2022-11-22 17:44:44 -0500

    Hints for 3rd tab

--- app.R
+++ app.R
@@ -1,14 +1,19 @@
 library(shiny)
 library(shinythemes)
 library(plotly)
-library(tidyverse)
-library(corrr)
 
 # Loading datasets
 library(palmerpenguins)
 data(penguins)
 
-ui <- fluidPage(theme = shinytheme("darkly"),
+###################################################################
+### WE NEED TO LOAD TWO LIBRARIES 
+library(tidyverse)
+library(     ) ## Library for working with correlate() and rplot()
+###################################################################
+
+
+ui <- fluidPage(theme = shinytheme("united"),   #try: united, superhero, ...
         navbarPage("Penguins data",
           tabPanel("Linear Regression",
             sidebarPanel(
@@ -40,26 +45,51 @@ ui <- fluidPage(theme = shinytheme("darkly"),
               plotOutput("hist")
             )
           ),
-      
-          tabPanel("Matrix correlation","Loading!!!")
-          
+############################################################################
+          tabPanel("Correlation Matrix",
+            sidebarPanel(
+              tags$h3("Correlation Matrix"),
+              shiny::selectInput(
+                inputId = "v3",
+                label = "Select grouping variable",
+                choices = c("Sex" = "sex",          ## Name the variables 
+                            "Species" = "#fill# ",  ## inputs (column names)
+                            "Island" = "#fill#")    ## the user can choose.
+              ),
+            ),
+            mainPanel(
+              h3("Correlation matrix grouping penguins body mass per variable selected"),
+              plotOutput("#fill#")    ## fill w/output name (from 3rd output name)
+            )
+          ),
+############################################################################
         )
 )
 
 server <- function(input, output) {
 
   output$lmreg <- renderPlotly({
-    cc <- ggplot(aes(x=get(input$v1),y=body_mass_g),data=penguins)+
+    ggplot(aes(x=get(input$v1),y=body_mass_g),data=penguins)+
       geom_point() +
       geom_abline(slope = 1, intercept = 0) +
       ylab("Body Mass (gr.)") + xlab("Variable selected (mm)")
-    
   })
   output$hist <- renderPlot({
     bodymass <- na.omit(penguins$body_mass_g)
     htg <- seq(min(bodymass),max(bodymass),length.out = input$v2)
     hist(bodymass, breaks = htg, xlab = 'Body Mass (gr.)',col="red",border = "black",main = NULL)
   })
+##############################################################################
+  output$cormatrix <- renderPlot({  ## This is the 3rd output that should be referenced in the main panel
+    penguins0 <- na.omit(penguins)
+    new_peng <- #fill# %>%     ## fill with the dataset name from previous row
+      pivot_wider(names_from = #fill w/input3#, values_from = body_mass_g,names_prefix = "body mass ") %>%
+      select_if(is.numeric) %>%
+      select(-year) %>%
+      correlate()
+    rplot(#fill#)   ##fill with correlation object just created
+  })
+##############################################################################  
 }
 
 shinyApp(ui = ui, server = server)
